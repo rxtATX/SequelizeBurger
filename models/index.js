82b159af0b31f87f97d216338a1243caac2e6f90
rxtATX <rachel.thiim@gmail.com>
2017-02-21 20:02:40 -0600

    Config whyyy

--- models/index.js
+++ models/index.js
@@ -5,9 +5,9 @@ var path = require('path');
 var Sequelize = require('sequelize');
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || 'development';
-var config = require(__dirname + '/config/config.json')[env];
+var config = require(__dirname + '../../../config/config.json')[env];
 var db = {};
-
+var config = require('/../../../config/config.json');
 // var passphrase= require("../keys.js");
 
 // if(env==="development"){
